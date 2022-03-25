5c75153d02aa39f5fbdc676d78ba8b216e86444e
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-25 16:53:02 -0300

    refactor: create helper a function to create the matrix

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,28 @@ const MatrixGenerator = (valueInMiddle: number) => {
     return [1];
   }
 
-  return [
-    [1, 1, 1],
-    [1, 2, 1],
-    [1, 1, 1],
-  ];
+  if (valueInMiddle === 2) {
+    const middleIndex = valueInMiddle - 1;
+    const { matrix } = createMatrix(valueInMiddle)(1);
+    matrix[middleIndex][middleIndex] = 2;
+
+    return matrix;
+  }
+};
+
+const createMatrix = (valueInMiddle: number) => (initialValue: number) => {
+  const queueCount = 3;
+  const matrix = Array.from(
+    { length: queueCount }, //
+    (_, i) =>
+      Array.from(
+        { length: queueCount }, //
+        (_, j) => initialValue,
+      ),
+  );
+
+  return {
+    queueCount,
+    matrix,
+  };
 };
